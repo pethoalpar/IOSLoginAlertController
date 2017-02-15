f4ee7471001aa32716a2c81997d3453ff4cedd24
Petho Alpar <pethoalpar@192-168-0-102.rdsnet.ro>
2017-02-15 21:26:10 +0200

    First commit

--- LoginWithAlertController/ViewController.swift
+++ LoginWithAlertController/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func doLogin(_ sender: Any) {
+        
+        let userName = userNameField.text
+        let password = passwordField.text
+        
+        let alertController = UIAlertController(title: "Login Status", message: userName == "demo" && password == "demo" ? "Success" : "Failed", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alertController,animated: true, completion: nil)
+    }
 
 }
 
